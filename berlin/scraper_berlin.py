0e790dbf82a33d98a6b850d02c3061218fe0ee7b
nwuensche <Wuensche.Niklas@gmail.com>
2019-03-13 11:36:51 +0100

    Copy Methods Bremen

--- berlin/scraper_berlin.py
+++ berlin/scraper_berlin.py
@@ -26,20 +26,92 @@ def get_pdf_urls():
         link = field.xpath('div[3]/a')[0]
         yield num, BASE_URL + link.attrib['href']
 
+def reformat_top_num(top_num):
+    try:
+        num = int(top_num)
+        return str(num) + "."
+    except ValueError: # Happens when top_num e.g. 48 a or 56 b
+        return '{} {}'.format(top_num[:-1]+ ".", top_num[-1] + ")")
+
+def get_reformatted_tops(top_nums):
+    return [reformat_top_num(t) for t in top_nums]
 
 def get_beschluesse_text(session, filename):
     cutter = pdfcutter.PDFCutter(filename=filename)
     session_number = int(session['number'])
 
     top_nums = [t['number'] for t in session['tops'] if t['top_type'] == 'normal'] # 1, 2, 3a, 3b, 4,....
-#    reformatted_top_nums = get_reformatted_tops(top_nums) #1., 2., 3. a), 3. b), 4.,...
-    return {}
+    reformatted_top_nums = get_reformatted_tops(top_nums) #1., 2., 3. a), 3. b), 4.,...
 
+    #e.g. "1b", ("1. b)", "2.")
+    for top_num, (current, next_) in zip(top_nums, helper.with_next(reformatted_top_nums)):
+#        if('a)' in current):
+#            current_top = cutter.filter(auto_regex='^{}\.$'.format(current.split()[0][:-1])) #46. a) -> 46
+        if(')' in current):
+            #e.g. 46. a) problem: 46 isn't part of the TOP anymore. There, find the first line below 46 that is starting with b)
+            current_top = cutter.filter(auto_regex='^{}'.format(current.split()[-1].replace(')', '\\)'))) #46. b) -> b\) because of regex brackets
+            curr_num = current.split()[0] #46. b) -> 46.
+#            current_num = cutter.filter(auto_regex='^{}$'.format(curr_num)) #Find line beginning with 46.
+            current_num = cutter.filter(auto_regex='^{}\.$'.format(curr_num[:-1])) #Find line beginning with 46
+            current_top = current_top.below(current_num)[0] #Find first line starting with b) below TOP 46.
+        else:
+            current_top = cutter.filter(auto_regex='^{}\.$'.format(current[:-1])) #Escape . in 46. because of regex
 
+        next_top = None
+        if next_ is not None: #There is a TOP after this one which we have to take as a lower border
+            #Exactly the same as for the current_top
+#            if('a)' in next_):
+#                next_top = cutter.filter(auto_regex='^{}\.$'.format(next_.split()[0][:-1]))
+            if(')' in next_):
+                next_top = cutter.filter(auto_regex='^{}'.format(next_.split()[-1].replace(')', '\\)')))
+                next_top = next_top.below(current_top)[0] #Don't have to find TOP number line, because we can use current_top as a upper border
+            else:
+                next_top = cutter.filter(auto_regex='^{}\.$'.format(next_[:-1]))
+        senats_text, br_text = getSenatsAndBrTextsForCurrentTOP(cutter, current_top, next_top)
+        yield top_num, {'senat': senats_text, 'bundesrat': br_text}
 
+def getSenatsAndBrTextsForCurrentTOP(cutter, current_top, next_top):
+    column_two = 731 #start of third (and last) column on type 2 docs, don't need anything from this third column, so just look at the stuff left from it
+    page_heading = 135 #Right under the grey heading of each table
+    page_number = 735 #Bottom of each page
+    right_title = 550
+    right_senat = 850
+    right_br = 1150
+#    senats = cutter.filter(auto_regex='^Senats-?') | cutter.filter(auto_regex='^Beschluss$')
+    senats = cutter.filter(auto_regex='.*').below(current_top)
+    if next_top:
+        senats = senats.above(next_top)
+
+    ergebnis_br = cutter.filter(auto_regex='.*').below(current_top)
+
+    if next_top:
+        ergebnis_br = ergebnis_br.above(next_top)
+
+    senats_text = cutter.all().filter(
+        doc_top__gte=senats.doc_top -15 ,
+        top__gte=page_heading,
+        bottom__lt=page_number,
+        left__gt=right_title,
+        right__lt=right_senat
+    )
+
+    br_text = cutter.all().filter(
+        doc_top__gte=ergebnis_br.doc_top -15 ,#Relative to all pages, biggest offset in 938.19
+        top__gte=page_heading,
+        bottom__lt=page_number,
+        left__gt=right_senat,
+        right__lt=right_br
+    )
+
+    if next_top:
+        br_text = br_text.above(next_top)
+    #senats_text = senats_text.right_of(senats)
+    #br_text = br_text.right_of(ergebnis_br)
+    return senats_text.clean_text(), br_text.clean_text()
 
 def get_session(session):
     PDF_URLS = dict(get_pdf_urls())
+    PDF_URLS = {973: PDF_URLS[973]}
     try:
         filename = helper.get_session_pdf_filename(session, PDF_URLS)
     except KeyError:
