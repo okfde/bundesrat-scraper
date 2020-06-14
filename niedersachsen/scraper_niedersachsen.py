880fc1108fb1a51e918d349b89cc69924485b2e6
nwuensche <Wuensche.Niklas@gmail.com>
2020-06-14 16:17:10 +0200

    Intertwine I/O session number and Parse Rules/Output

--- niedersachsen/scraper_niedersachsen.py
+++ niedersachsen/scraper_niedersachsen.py
@@ -20,6 +20,29 @@ LINK_TEXT_RE = re.compile(r'Abstimmungsverhalten und Beschlüsse vom.*') #In 202
 SENAT_TEXT_RE = re.compile(r'^Haltung NI\s*:\s*(.*)')
 BR_TEXT_RE = re.compile(r'^Ergebnis BR\s*:\s*(.*)')
 
+def get_pdf_urls():
+    #Normal parsing is to hard because HTML is not formatted consistently
+    response = requests.get(INDEX_URL)
+    root = etree.fromstring(response.content)
+    lines = re.split('\n|<br>', response.text)# For 2020, Niedersachsen doesn't break lines anymore between Sitzungen, but rather uses only <br>. Split on both
+    meeting_nums = [NUM_RE.search(l).group(1) for l in lines if NUM_RE.search(l)]
+    pdf_links = [a.attrib['href'] for a in root.xpath('//a') if a.text != None and LINK_TEXT_RE.search(a.text)]
+    for (num, link) in zip(meeting_nums, pdf_links):
+        yield int(num), link
+
+#TODO Everywhere in Helper?
+#46a -> 46. a)
+def reformat_top_num(top_num):
+    try:
+        num = int(top_num)
+        return str(num) + "."
+    except ValueError: # Happens when top_num e.g. 48 a or 56 b
+        return '{} {}'.format(top_num[:-1]+ ".", top_num[-1] + ")")
+
+#TODO Everywhere in Helper?
+def get_reformatted_tops(top_nums):
+    return [reformat_top_num(t) for t in top_nums]
+
 #Rules for parsing pdfs for Senats and BR text per TOP
 #Mostly used as Namespace
 #Rules different for each County, so Counties can derive this class
@@ -118,3 +141,92 @@ class SelectionRules970MultiDigitNumber2(SelectionRules970MultiDigitNumber):
 
         # Do same as for 10-97 with different split of number
         return self._getNumberSelectionSplittedNumber(firstTwoDigits, lastPartNumber)
+
+def get_beschluesse_text_type(session, filename, type_num):
+    #print(filename)
+    cutter = pdfcutter.PDFCutter(filename=filename)
+    session_number = int(session['number'])
+
+    top_nums = [t['number'] for t in session['tops'] if t['top_type'] == 'normal'] # 1, 2, 3a, 3b, 4,....
+    reformatted_top_nums = get_reformatted_tops(top_nums) #1., 2., 3. a), 3. b), 4.,...
+
+    #e.g. "1b", ("1. b)", "2.")
+    for top_num, (current, next_) in zip(top_nums, helper.with_next(reformatted_top_nums)):
+        selectionCurrentTOP = getTOPSelectionWithRightRules(cutter, current, session_number)
+
+        selectionNextTOP = None
+        if next_ is not None: #There is a TOP after this one which we have to take as a lower border
+            #Exactly the same as for the current_top
+            selectionNextTOP = getTOPSelectionWithRightRules(cutter, next_, session_number)
+        senats_text, br_text = getSenatsAndBrTextsForCurrentTOP(cutter, selectionCurrentTOP, selectionNextTOP)
+        yield top_num, {'senat': senats_text, 'bundesrat': br_text}
+
+# Decide if I need custom rules for special session/TOP cases because PDF format isn't consistent
+def getTOPSelectionWithRightRules(cutter, top, sessionNumber):
+    if sessionNumber == 970 and 10 <= int(top.split()[0][:-1]) <= 97:
+        return SelectionRules970MultiDigitNumber(cutter).getTOPSelection(top)
+    elif sessionNumber == 970 and int(top.split()[0][:-1]) >= 98 :
+        return SelectionRules970MultiDigitNumber2(cutter).getTOPSelection(top)
+    return SelectionRules(cutter).getTOPSelection(top)
+
+def getSenatsAndBrTextsForCurrentTOP(cutter, current_top, next_top):
+    page_heading = 73 #Bottom of heading on each page
+    page_footer = 1260 #Upper of footer on each page
+    senats = cutter.filter(auto_regex='^Haltung NI')
+    senats = senats.below(current_top)
+    if next_top:
+        senats = senats.above(next_top)
+
+    #INFO Space relevant because without Rules broke because of "Ergebnisse" in NS 970 70a
+    #INFO But can't filter 'Ergebnis BR' directly, because these two words are sometimes in different chunks
+    ergebnis_br = cutter.filter(auto_regex='^Ergebnis ').below(current_top)
+
+    if next_top:
+        ergebnis_br = ergebnis_br.above(next_top)
+
+    #cutter.above() is strict, can get it non strict by going a little bit higher
+    senats_text = cutter.all().filter(
+        doc_top__gte=senats.doc_top - 1 ,
+        top__gte=page_heading,
+        bottom__lt=page_footer,
+    )
+
+    br_text = cutter.all().filter(
+        doc_top__gte=ergebnis_br.doc_top - 1 ,#Relative to all pages
+        top__gte=page_heading,
+        bottom__lt=page_footer,
+    )
+
+    if next_top:
+        br_text = br_text.above(next_top)
+        senats_text = senats_text.above(ergebnis_br)
+
+
+    #Cut away "Haltung NI:" and "Ergebnis BR:" from text
+#    print("current_top", current_top.clean_text())
+    senats_text = senats_text.clean_text()
+#    print("next top", next_top)
+#    print("senats_text", senats_text)
+    if senats_text != "":
+        senats_text = SENAT_TEXT_RE.search(senats_text).group(1)
+
+    br_text = br_text.clean_text()
+#    print("br_text", br_text)
+    if br_text != "":
+        br_text = BR_TEXT_RE.search(br_text).group(1)
+    return senats_text, br_text
+
+def get_beschluesse_text(session, filename):
+    session_number = int(session['number'])
+    if session_number >= 973:
+        return get_beschluesse_text_type(session, filename, 1)
+    else:
+        return get_beschluesse_text_type(session, filename, 2)
+
+def get_session(session):
+    PDF_URLS = dict(get_pdf_urls())
+    try:
+        filename = helper.get_session_pdf_filename(session, PDF_URLS)
+    except KeyError:
+        return
+    return dict(get_beschluesse_text(session, filename))
