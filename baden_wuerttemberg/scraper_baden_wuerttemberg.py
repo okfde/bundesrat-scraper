0db46efc247371aaccc34584ea02dc3fb78c9d0b
nwuensche <Wuensche.Niklas@gmail.com>
2020-06-22 20:30:30 +0200

    Parse BW with new Classes

---
+++ baden_wuerttemberg/scraper_baden_wuerttemberg.py
@@ -0,0 +1,70 @@
+import re
+import pdb
+
+import requests
+from lxml import html as etree
+
+import pdfcutter
+
+# Import relative Parent Directory for Helper Classes
+import os, sys
+sys.path.insert(0, os.path.abspath('..')) #Used when call is ` python3 file.py`
+sys.path.insert(0, os.path.abspath('.')) #Used when call is ` python3 $COUNTY/file.py`
+import helper
+import selectionVisualizer as dVis
+import PDFTextExtractor
+import MainBoilerPlate
+
+INDEX_URL = 'https://stm.baden-wuerttemberg.de/de/vertretung-beim-bund/service-und-presse/bundesratsinitiativen/'
+NUM_RE = re.compile(r'(\d+)\. Sitzung des Bundesrates am .*')
+
+class MainExtractorMethod(MainBoilerPlate.MainExtractorMethod):
+
+    #Out: Dict of {sessionNumberOfBR: PDFWebLink} entries
+    def _get_pdf_urls(self):
+        response = requests.get(INDEX_URL)
+        root = etree.fromstring(response.content)
+        names = root.xpath('//*[@id="accordion-item-489-content"]/div/p/a')
+        for name in names:
+            text = name.text_content()
+            num = int(NUM_RE.search(text).group(1))
+            link = name.attrib['href']
+
+            yield int(num), link
+
+#Senats/BR Texts and TOPS in BW  all have same formatting
+class SenatsAndBRTextExtractor(PDFTextExtractor.AbstractSenatsAndBRTextExtractor):
+    def _extractSenatBRTexts(self, selectionCurrentTOP, selectionNextTOP):
+        page_heading = 73 #Bottom of heading on each page
+        page_footer = 1260 #Upper of footer on each page
+
+        #Get indented Text, Senats text is everything below it, need to check below this because otherwise I also filter Name of TOP
+        TOPRightIndented = self.cutter.all().below(selectionCurrentTOP).filter(
+            left__gte = selectionCurrentTOP.left + 100 
+        )
+
+        if selectionNextTOP:
+            TOPRightIndented = TOPRightIndented.above(selectionNextTOP)
+
+        last_indented_with_text = None
+        #empty, but present lines below senat text can mess up parsing, so only watch for last non-empty
+        for line in TOPRightIndented:
+            if line.clean_text(): #empty strings are falsy
+                last_indented_with_text = line
+
+        senats_text = self.cutter.all().below(last_indented_with_text)
+        if selectionNextTOP:
+            senats_text = senats_text.above(selectionNextTOP)
+
+        senats_text = senats_text.clean_text()
+
+        br_text = "" #BW doesnt repeat BR Text in its PDFs
+        return senats_text, br_text
+
+#Senats/BR Texts and TOPS in BW  all have same formatting
+class NSTextExtractorHolder(PDFTextExtractor.TextExtractorHolder):
+    # Decide if I need custom rules for special session/TOP cases because PDF format isn't consistent
+    #In BW all Text Rules are consistent
+    def _getRightSenatBRTextExtractor(self, top, cutter): 
+        return SenatsAndBRTextExtractor(cutter)
+
