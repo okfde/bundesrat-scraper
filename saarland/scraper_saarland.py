6aba184ea1da4161f8a858bc1786be0ef801f73c
nwuensche <Wuensche.Niklas@gmail.com>
2020-06-28 13:33:01 +0200

    Add Text Extractor Saarland

--- saarland/scraper_saarland.py
+++ saarland/scraper_saarland.py
@@ -38,3 +38,47 @@ class MainExtractorMethod(MainBoilerPlate.MainExtractorMethod):
                 continue
             link = BASE_URL + partLink
             yield int(num), link
+
+class TOPPositionFinder(PDFTextExtractor.DefaultTOPPositionFinder):
+
+    #SL formats .e.g. 973 25 a) as 25a. So use different format and only search for this 25a. alone (without number search before)
+    def _getTOPSubpartSelection(self, top):
+        number, subpart = top.split() #46. b) -> [46., b)]
+        TOPRightFormat = number[:-1] + subpart.replace(")", ".") # 46. b) -> 46b.
+        topSelection = self._getNumberSelection(TOPRightFormat) #Although not only a number, this still works
+        return topSelection
+
+#Senats/BR Texts and TOPS in BW  all have same formatting
+class SenatsAndBRTextExtractor(PDFTextExtractor.AbstractSenatsAndBRTextExtractor):
+
+    def _extractSenatBRTexts(self, selectionCurrentTOP, selectionNextTOP):
+        page_heading = 73 #Bottom of heading on each page
+        page_footer = 1260 #Upper of footer on each page
+
+        #Get indented Text, Senats/BR text is everything below it, need to check below this because otherwise I also filter Name of TOP
+
+        #Because selectionNextTOP is never empty (but could be empty selector), I can use it without checking if it is None or empty
+        senatTitleSelection = self.cutter.all().filter(auto_regex="^Haltung SL:").below(selectionCurrentTOP).above(selectionNextTOP)
+        BRTitleSelection = self.cutter.all().filter(auto_regex="^Ergebnis BR:").below(selectionCurrentTOP).above(selectionNextTOP)
+
+        senats_text = self.cutter.all().filter(
+                doc_top__gte = senatTitleSelection.doc_top-1 #Senats Text starts next to title
+        ).above(BRTitleSelection).clean_text()
+        br_text = self.cutter.all().filter(
+                doc_top__gte = BRTitleSelection.doc_top-1, #BR Text starts next to title
+                auto_regex="^[^_]+"
+        ).above(selectionNextTOP).clean_text()
+
+        # Although in 973 TOP 9, senats_text ends with many blank lines, in JSON they are somehow striped
+        return senats_text, br_text
+
+#Senats/BR Texts and TOPS in SL all have same formatting
+class TextExtractorHolder(PDFTextExtractor.TextExtractorHolder):
+
+    def _getRightTOPPositionFinder(self, top):
+        return TOPPositionFinder(self.cutter)
+
+    # Decide if I need custom rules for special session/TOP cases because PDF format isn't consistent
+    #In BW all Text Rules are consistent
+    def _getRightSenatBRTextExtractor(self, top, cutter): 
+        return SenatsAndBRTextExtractor(cutter)
