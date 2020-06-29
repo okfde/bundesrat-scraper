883c5d1ac137dd7d354c5ed6c0b41ad19a454a95
nwuensche <Wuensche.Niklas@gmail.com>
2020-06-29 11:03:39 +0200

    Extract Texts PDFs SH

--- schleswig_holstein/scraper_schleswig_holstein.py
+++ schleswig_holstein/scraper_schleswig_holstein.py
@@ -59,3 +59,40 @@ class MainExtractorMethod(MainBoilerPlate.MainExtractorMethod):
             redirectATag = redirectRoot.xpath('//a[contains(@href, "pdf")]')[0] #Somehow, XPath from Chrome doesn't find anything here, so search inside href for "pdf" substring
             link = BASE_URL + redirectATag.attrib['href']
         return num, link
+
+#Senats/BR Texts and TOPS in SH all have same formatting
+class SenatsAndBRTextExtractor(PDFTextExtractor.AbstractSenatsAndBRTextExtractor):
+    def _extractSenatBRTexts(self, selectionCurrentTOP, selectionNextTOP):
+        page_heading = 73 #Bottom of heading on each page
+        page_footer = 1260 #Upper of footer on each page
+
+        #Get indented Text, Senats text is everything below it, need to check below this because otherwise I also filter Name of TOP
+        TOPRightIndented = self.cutter.all().below(selectionCurrentTOP).filter(
+            left__gte = selectionCurrentTOP.left + 100 
+        )
+
+        if selectionNextTOP:
+            TOPRightIndented = TOPRightIndented.above(selectionNextTOP)
+
+        last_indented_with_text = None
+        #empty, but present lines below senat text can mess up parsing, so only watch for last non-empty
+        for line in TOPRightIndented:
+            if line.clean_text(): #empty strings are falsy
+                last_indented_with_text = line
+
+        senats_text = self.cutter.all().below(last_indented_with_text)
+        if selectionNextTOP:
+            senats_text = senats_text.above(selectionNextTOP)
+
+        senats_text = senats_text.clean_text()
+
+        br_text = "" #SH doesnt repeat BR Text in its PDFs
+        return senats_text, br_text
+
+#Senats/BR Texts and TOPS in SH  all have same formatting
+class TextExtractorHolder(PDFTextExtractor.TextExtractorHolder):
+    # Decide if I need custom rules for special session/TOP cases because PDF format isn't consistent
+    #In BW all Text Rules are consistent
+    def _getRightSenatBRTextExtractor(self, top, cutter): 
+        return SenatsAndBRTextExtractor(cutter)
+
