abdb8600a9ed804fc737367d1c202ae290f8dc36
nwuensche <Wuensche.Niklas@gmail.com>
2020-07-09 16:16:06 +0200

    Extract Texts SA

--- sachsen_anhalt/scraper_sachsen_anhalt.py
+++ sachsen_anhalt/scraper_sachsen_anhalt.py
@@ -36,3 +36,94 @@ class MainExtractorMethod(MainBoilerPlate.MainExtractorMethod):
             num = int(maybeNum.group(1))
             link = name.attrib['href']
             yield int(num), link
+
+#SA uses "46" or "23a" as TOPs instead of "46." and "23. a)"
+class TOPPositionFinder(PDFTextExtractor.DefaultTOPPositionFinder):
+    def _getTOPSubpartSelection(self, top):
+        number, subpart = top.split() #46. b) -> [46., b)]
+        formatedTOP = number[:-1] + subpart[:-1] #[46., b)] -> 46b
+        topSelection = self._getNumberSelection(formatedTOP) #Not only number, but still works, bu
+        topSelection = topSelection.filter(
+                left__lte = 160 # Dont match e.g. "980 Sitzung" in title for TOP 9 (happens in 989 TOP 9)
+                )
+        return topSelection
+
+    def _getNumberSelection(self, number):
+        formatedNumber = number.split(".")[0] #46. -> 46 , Done with split because also use it here in SA for Subparts as well
+        escapedNum = helper.escapeForRegex(formatedNumber)
+        allSelectionsNumber = self.cutter.filter(auto_regex='^{}'.format(escapedNum)).filter(
+                left__lte = 160 # Dont match e.g. "980 Sitzung" in title for TOP 9 (happens in 989 TOP 9)
+                )
+        return self._getHighestSelection(allSelectionsNumber)
+
+
+class SenatsAndBRTextExtractor(PDFTextExtractor.AbstractSenatsAndBRTextExtractor):
+
+    #First Extract BR Text and only after this Senat Text, because SA sometimes uses "Ergebnis BR / Abstimmung ST:" Combo Blocks and BR Text always above senats text
+    def _extractSenatBRTexts(self, selectionCurrentTOP, selectionNextTOP):
+        page_heading = 123 #Bottom of heading on each page
+        page_footer = 1260 #Upper of footer on each page
+
+
+        ergebnis_br = self.cutter.filter(auto_regex='^Ergebnis BR').below(selectionCurrentTOP).above(selectionNextTOP)
+
+        br_text = self.cutter.all().filter(
+            doc_top__gte=ergebnis_br.doc_top - 1 ,#Relative to all pages
+            top__gte=page_heading, #Remove Page header if in current selecion
+            bottom__lt=page_footer,
+        )
+
+        if selectionNextTOP:
+            br_text = br_text.above(selectionNextTOP)
+
+        if "Abstimmung ST" in ergebnis_br.clean_text(): #"Ergebnis BR / Abstimmung ST:" Combo Block -> Senat + BR Text are same
+
+            br_text = br_text.clean_text()
+            weg = br_text
+            maybe_br_text = BRSENAT_TEXT_RE.search(br_text)
+            if maybe_br_text:
+                br_text = maybe_br_text.group(1)
+            else:
+                br_text = ""
+            senats_text = br_text
+
+        else: #Separat Senats Text
+            abstimmung_st_senat = self.cutter.filter(auto_regex='^Abstimmung ST').below(selectionCurrentTOP).above(selectionNextTOP)
+
+            senats_text = self.cutter.all().filter(
+                doc_top__gte=abstimmung_st_senat.doc_top - 1 ,#Relative to all pages
+                top__gte=page_heading, #Remove Page header if in current selecion
+                bottom__lt=page_footer,
+            )
+            if selectionNextTOP:
+                senats_text = senats_text.above(selectionNextTOP)
+
+            if abstimmung_st_senat: #If no senat text, then .above() would somehow select nothing, which is not what we want
+                br_text = br_text.above(abstimmung_st_senat) #For some reason, .above(senats_text) sometimes returns empty selection
+
+            # Filter out "Ergebnis BR" Prefix
+            br_text = br_text.clean_text()
+            maybe_br_text = BR_TEXT_RE.search(br_text)
+            if maybe_br_text:
+                br_text = maybe_br_text.group(1)
+            else:
+                br_text = ""
+
+
+
+            # Filter out "Abstimmung ST:" Prefix
+            senats_text = senats_text.clean_text()
+            maybe_senats_text = SENAT_TEXT_RE.search(senats_text)
+            if  maybe_senats_text:
+                senats_text = maybe_senats_text.group(1)
+            else:
+                senats_text = ""
+
+        return senats_text, br_text
+
+class TextExtractorHolder(PDFTextExtractor.TextExtractorHolder):
+    def _getRightSenatBRTextExtractor(self, top, cutter):
+        return SenatsAndBRTextExtractor(cutter)
+
+    def _getRightTOPPositionFinder(self, top):
+        return TOPPositionFinder(self.cutter)
