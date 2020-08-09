0a49c0e2df0ceb694b1c357f6d51f2f312dfc969
nwuensche <Wuensche.Niklas@gmail.com>
2020-08-09 11:44:52 +0200

    Fix SAARLAND 992 88 missing text

--- saarland/scraper_saarland.py
+++ saarland/scraper_saarland.py
@@ -73,16 +73,27 @@ class SenatsAndBRTextExtractor(PDFTextExtractor.AbstractSenatsAndBRTextExtractor
         #Get indented Text, Senats/BR text is everything below it, need to check below this because otherwise I also filter Name of TOP
 
         #Because selectionNextTOP is never empty (but could be empty selector), I can use it without checking if it is None or empty
-        senatTitleSelection = self.cutter.all().filter(auto_regex="^Haltung SL:").below(selectionCurrentTOP).above(selectionNextTOP)
-        BRTitleSelection = self.cutter.all().filter(auto_regex="^Ergebnis BR:").below(selectionCurrentTOP).above(selectionNextTOP)
+        senatTitleSelection = self.cutter.all().filter(auto_regex="^Haltung SL:").below(selectionCurrentTOP)
+        BRTitleSelection = self.cutter.all().filter(auto_regex="^Ergebnis BR:").below(selectionCurrentTOP)
+        if selectionNextTOP: #Otherwise Always empty text if no next TOP
+            senatTitleSelection = senatTitleSelection.above(selectionNextTOP)
+            BRTitleSelection = BRTitleSelection.above(selectionNextTOP)
 
         senats_text = self.cutter.all().filter(
                 doc_top__gte = senatTitleSelection.doc_top-1 #Senats Text starts next to title
-        ).above(BRTitleSelection).clean_text()
+        )
+        if BRTitleSelection: #Otherwise Always empty text if BRTitleSelection empty
+            senats_text = senats_text.above(BRTitleSelection)
+
         br_text = self.cutter.all().filter(
                 doc_top__gte = BRTitleSelection.doc_top-1, #BR Text starts next to title
                 auto_regex="^[^_]+" #Each TOP ends with ----- line, filter this one out
-        ).above(selectionNextTOP).clean_text()
+        )
+        if selectionNextTOP: #Otherwise Always empty text if no next TOP
+            br_text = br_text.above(selectionNextTOP)
+
+        senats_text = senats_text.clean_text()
+        br_text = br_text.clean_text()
 
         # Although in 973 TOP 9, senats_text ends with many blank lines, in JSON they are somehow striped
         return senats_text, br_text
