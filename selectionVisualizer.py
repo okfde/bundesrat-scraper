985fb1ef44b3e85b3fa44ec39e376cea4807d003
nwuensche <Wuensche.Niklas@gmail.com>
2020-09-11 17:04:45 +0200

    Fix some TODOs

--- selectionVisualizer.py
+++ selectionVisualizer.py
@@ -9,8 +9,6 @@ def showCutter(page,  pageNumber=None):
     d1 = debugger.debug(page, page=pageNumber) #INFO Really need this pagenumber here, else DEBUG: FC_WEIGHT didn't match error here
     #page = cutter.filter(page=pageNumber)
     d = d1.get_page_as_html(pageNumber) # gibt dir bild zurück
-    #TODO Tmp absolute file
-#    outfile = "TMPselected.html"
     outfile = subprocess.check_output("mktemp")[:-1].decode("ascii") #Remove \n at the end + bytes to str (Needed for e.g. mktemp)
     with open(outfile, "w") as file:
         file.write(d)
