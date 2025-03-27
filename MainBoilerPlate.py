25a367648cc55f2fd53c54dc43e4e7733f8b2ceb
nwuensche <gh@nwuensche.com>
2025-03-27 11:54:54 +0100

    add

--- MainBoilerPlate.py
+++ MainBoilerPlate.py
@@ -13,18 +13,32 @@ class MainExtractorMethod:
     #In: Session Dict
     #Out: Dict of "TOP: {'senat': senatsText, 'bundesrat': BRText}" entries
     def get_session(self, session):
-        PDF_URLS = dict(self._get_pdf_urls())
-
+        from_json=True
         URLFILENAME = "session_urls.json"
-        if not os.path.exists(URLFILENAME): #Create PDF Link JSON File
-            with open(URLFILENAME, 'w') as f: #Because of override of MainExtractorMethod in counties, the FILENAME is always relative to folder
+        PDF_URLS = {}
+        
+        # First try to load URLs from JSON file if from_json is True
+        if from_json and os.path.exists(URLFILENAME):
+            try:
+                with open(URLFILENAME, 'r') as f:
+                    PDF_URLS = json.load(f)
+            except json.JSONDecodeError:
+                # If JSON file is invalid, fall back to _get_pdf_urls()
+                PDF_URLS = dict(self._get_pdf_urls())
+        
+        # If URL not found in JSON or from_json is False, get URLs from source
+        if not PDF_URLS or str(session['number']) not in PDF_URLS:
+            PDF_URLS = dict(self._get_pdf_urls())
+            
+            # Create or update the PDF Link JSON File
+            with open(URLFILENAME, 'w') as f:
                 json.dump(PDF_URLS, f)
-
 
         try:
             filename = helper.get_session_pdf_filename(session, PDF_URLS)
         except KeyError:
             return
+        print(filename)
         return self.get_beschluesse_text(session, filename)
 
     #Out: Dict of {sessionNumberOfBR: PDFWebLink} entries
@@ -37,5 +51,3 @@ class MainExtractorMethod:
     def get_beschluesse_text(self, session, filename):
         extractor = self.textExtractorHolderSubclass(filename, session)
         return dict(extractor.getSenatsAndBRTextsForAllSessionTOPs())
-
-
