4d3bded2f56b0adef49ffb55393862cb66d3a3d8
nwuensche <gh@nwuensche.com>
2026-02-02 06:20:32 +0100

    first few

--- main.sh
+++ main.sh
@@ -11,26 +11,29 @@ fi
 
 source $VENV/bin/activate
 echo "Install requirements"
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
-#echo "Load new Sessions from bundesrat"
+echo "Load new Sessions from bundesrat"
+set -x
 #(cd bundesrat && trash sessions.json &&  python bundesrat_scraper.py)
 
 
-(cd bayern && trash session_urls.json &&  python scraper.py)
-(cd berlin && trash session_urls.json &&  python scraper.py)
-(cd brandenburg && trash session_urls.json &&  python scraper.py)
-(cd bremen && trash session_urls.json &&  python scraper.py)
-(cd hamburg && trash session_urls.json &&  python scraper.py)
-(cd hessen && trash session_urls.json &&  python scraper.py)
-(cd mecklenburg_vorpommern && trash session_urls.json &&  python scraper.py)
-(cd niedersachsen && trash session_urls.json &&  python scraper.py)
-(cd nordrhein_westfalen && trash session_urls.json &&  python scraper.py)
-(cd rheinland_pfalz && trash session_urls.json &&  python scraper.py)
-(cd saarland && trash session_urls.json &&  python scraper.py)
-(cd sachsen && trash session_urls.json &&  python scraper.py)
-(cd sachsen_anhalt && trash session_urls.json &&  python scraper.py)
+#(cd bayern && trash session_urls.json &&  python scraper.py)
+#(cd berlin && trash session_urls.json &&  python scraper.py)
+#(cd brandenburg && trash session_urls.json &&  python scraper.py)
+#(cd bremen && trash session_urls.json &&  python scraper.py)
+#(cd hamburg && trash session_urls.json &&  python scraper.py)
+#(cd hessen && trash session_urls.json &&  python scraper.py)
+#(cd mecklenburg_vorpommern && trash session_urls.json &&  python scraper.py)
+#(cd niedersachsen && trash session_urls.json &&  python scraper.py)
+#(cd nordrhein_westfalen && trash session_urls.json &&  python scraper.py)
+#(cd rheinland_pfalz && trash session_urls.json &&  python scraper.py)
+#(cd saarland && trash session_urls.json &&  python scraper.py)
+#(cd sachsen && trash session_urls.json &&  python scraper.py)
+#(cd sachsen_anhalt && trash session_urls.json &&  python scraper.py)
+(cd sachsen_anhalt &&  python scraper.py)
 (cd schleswig_holstein && trash session_urls.json &&  python scraper.py)
 (cd thueringen && trash session_urls.json &&  python scraper.py)
+set +x
 
 echo "Now commit, upload to okfde and restart the webservice"
